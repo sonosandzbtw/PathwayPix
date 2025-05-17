f9a2097e93200e8c63b8244d31c7c5af9e323eeb
sonosandzbtw <all4uddol2@gmail.com>
2025-05-17 19:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ st.markdown("""
 
         [data-testid="stAppViewBlockContainer"] {
             padding: 2rem 3rem;
-            background-color: #001E55  !important;
+            background-color: #1F618D   !important;
             color: #E3E9F0;
         }
     </style>
